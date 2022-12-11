17d9d54b42c7e0db83c53a2d823f710e57f71791
duck <godqhr2256@gmail.com>
2022-12-11 22:02:26 +0900

    feat: 세팅 완료

--- layouts/App.tsx
+++ layouts/App.tsx
@@ -1,7 +1,17 @@
-import React from 'react';
+import React, { FC } from 'react';
+import { Route, Routes } from 'react-router-dom';
+import loadable from '@loadable/component';
+/* 코드 스플리팅 */
+const Login = loadable(() => import('@pages/Login'));
+const SignUp = loadable(() => import('@pages/SignUp'));
 
-const App = () => {
-  return <div>testAPP!dd</div>;
+const App: FC = () => {
+  return (
+    <Routes>
+      <Route path="/login" element={<Login />} />
+      <Route path="/signup" element={<SignUp />} />
+    </Routes>
+  );
 };
 
 export default App;
---
+++ pages/SignUp/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { Form, Link } from 'react-router-dom';
+import { Header, Label, Input, Success, Button, LinkContainer } from './style';
+
+function SignUp() {
+  return <div>dd</div>;
+}
+
+export default SignUp;
