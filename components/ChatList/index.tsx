39ee8f7a60e2bd3f82d8054906e341e3f1ccf9c0
duck <godqhr2256@gmail.com>
2023-01-01 23:24:06 +0900

    feat: 채팅방 스크롤바 적용

--- components/ChatList/index.tsx
+++ components/ChatList/index.tsx
@@ -1,19 +1,22 @@
 import Chat from '@components/Chat';
-import React from 'react';
+import React, { useCallback, useRef } from 'react';
 import { IDM } from 'types/type';
 import { ChatZone, Section } from './style';
-
+import { Scrollbars } from 'react-custom-scrollbars';
 interface Props {
   chatData?: IDM[];
 }
 
 const ChatList = ({ chatData }: Props) => {
+  const onScroll = useCallback(() => {}, []);
+  const scrollbarRef = useRef(null);
   return (
     <ChatZone>
-      {/* <Section>section</Section> */}
-      {chatData?.map((chat) => (
-        <Chat key={chat.id} data={chat} />
-      ))}
+      <Scrollbars autoHide ref={scrollbarRef} onScrollFrame={onScroll}>
+        {chatData?.map((chat) => (
+          <Chat key={chat.id} data={chat} />
+        ))}
+      </Scrollbars>
     </ChatZone>
   );
 };
