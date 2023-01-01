18fdfaf9e65c7b66e2801ada360060270be75c2c
duck <godqhr2256@gmail.com>
2023-01-01 23:30:04 +0900

    feat: 채팅방 dayjs 적용

--- components/Chat/index.tsx
+++ components/Chat/index.tsx
@@ -2,19 +2,13 @@ import React from 'react';
 import { IDM } from 'types/type';
 import { ChatWrapper } from './style';
 import gravatar from 'gravatar';
-import useSWR from 'swr';
-import { useParams } from 'react-router';
-import fetcher from '@utils/fetcher';
+import dayjs from 'dayjs';
 
 interface Props {
   data: IDM;
 }
 
 function Chat({ data }: Props) {
-  //   const { workspace, id } = useParams<{ workspace: string; id: string }>();
-  //   const { data: user } = useSWR(`/api/workspaces/${workspace}/users/${id}`, fetcher, {
-  //     dedupingInterval: 2000,
-  //   });
   const user = data.Sender;
   return (
     <ChatWrapper>
@@ -24,7 +18,7 @@ function Chat({ data }: Props) {
       <div className="chat-text">
         <div className="chat-user">
           <b>{user.nickname}</b>
-          {/* <span>{data.createdAt}</span> */}
+          <span>{dayjs(data.createdAt).format('h:mm A')}</span>
         </div>
         <p>{data.content}</p>
       </div>
